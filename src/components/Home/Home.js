2a3c75edeb2c057b8932d37f6121f1303b06c7c1
GajendraTomar-07 <148423802+GajendraTomar-07@users.noreply.github.com>
2024-04-30 23:10:04 +0530

    t

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,6 @@
 import {Batch} from "../Batch/Batch";
 import {AllStories} from "../allstories/AllStories";
 import wave from '../../assets/wave.svg'
-import galaxy from '../../assets/galaxy two.jpg'
 import {Header} from "../header/Header";
 
 
@@ -31,7 +30,6 @@ export const Home = () => {
         <>
             <div className=" position-relative  pb-3 overflow-hidden" style={{minHeight: "70vh"}}>
                 <Header />
-                <img className=" position-absolute   w-100 h-100  " src={galaxy} style={{backgroundSize: "cover"}} alt="" />
                 <div className=" container  py-5 position-relative  z-1">
                     <h1 className=" py-5 mt-5 text-white text-center fw-bold">Science Fiction Stories </h1>
                     <div className="row  d-flex justify-content-around flex-wrap ">
@@ -44,9 +42,9 @@ export const Home = () => {
                         }
                     </div>
                 </div>
-                <div className=" w-100 position-absolute z-0 bottom-0">
+                {/* <div className=" w-100 position-absolute z-0 bottom-0">
                     <img src={wave} alt="" />
-                </div>
+                </div> */}
             </div>
             <div style={{backgroundColor: "#00042a"}}>
 
