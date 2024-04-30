1777120c3c9e312762b5e7260621ab50b060bbaa
GajendraTomar-07 <148423802+GajendraTomar-07@users.noreply.github.com>
2024-04-30 23:04:16 +0530

    t

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,28 @@
+
+import {Route,Routes} from "react-router-dom"
+// import {Layout} from "../components/Layout"
+import {Home} from "../components/Home/Home"
+import {StoreDetail} from "../components/StoriesDetails/StoreDetail"
+
+
+export const AppRoutes = () => {
+    return (
+        <div>
+            <Routes>
+                {/* route for first page  */}
+                <Route path="/" element={
+                    <>
+                        <Home />
+                    </>
+                } />
+                {/* route for second page  */}
+                <Route path="/stories/:id" element={
+                    <>
+                        <StoreDetail />
+                    </>
+                } />
+            </Routes>
+
+        </div>
+    )
+}
